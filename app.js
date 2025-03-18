bbb3a64a5ce088284cb4a38438f390b04d92d0af
Juan Espinosa <juan10torrespi@gmail.com>
2025-03-18 13:39:00 -0500

    Crea una función para agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 let listaAmigos = [];
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Ingresa un nombre válido.");
+        return;
+    }
+    
+    if (listaAmigos.includes(nombre)) {
+        alert("Este nombre ya está en la lista.");
+        return;
+    }
+    
+    listaAmigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
