c88a1f47dce41a50000c79db817426b0ab3b579d
Juan Espinosa <juan10torrespi@gmail.com>
2025-03-18 13:53:16 -0500

    Implementa una función para sortear el amigo secreto

--- app.js
+++ app.js
@@ -27,4 +27,16 @@ function actualizarLista() {
         lista.appendChild(li);
     }
 }
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("Debe haber al menos un amigo en la lista para hacer el sorteo.");
+        return;
+    }
+    
+    const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[indiceAleatorio];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${amigoSorteado}</strong></li>`;
+}
 
